2efa6cda227f08ceaef0bf143e3df3ce1d187992
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-10-13 14:27:17 -0400

    feature/postgres

---
+++ migrations/1570990669-create-database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE notes;
+
+-- Create a db trigger to set updated at
+CREATE OR REPLACE FUNCTION trigger_set_timestamp()
+    RETURNS TRIGGER AS $$
+BEGIN
+    NEW.updated_at = NOW();
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;

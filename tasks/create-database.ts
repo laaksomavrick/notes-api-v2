cf63cb97ce44d07896cff0219ae234e84669cc4d
Mavrick Laakso <laakso.mavrick@gmail.com>
2019-10-16 20:49:29 -0400

    bug/db-init (#11)

--- tasks/create-database.ts
+++ tasks/create-database.ts
@@ -5,16 +5,28 @@ import { LoggerFactory } from "../lib/logger";
     const logger = LoggerFactory.getLogger();
     // TODO: refactor config into lib/config alongside src/index.ts
     const database = new Database({
-        database: "notes",
+        database: "postgres",
         host: "localhost",
         port: 5432,
         user: "postgres",
     });
 
     await database.init();
-    await database.query("CREATE DATABASE notes");
+
+    await database.query("CREATE DATABASE notes;");
 
     logger.info("notes database created");
+
+    await database.query(`
+        CREATE TABLE notes.migrations(
+            id serial PRIMARY KEY,
+            migration_name VARCHAR(256) NOT NULL,
+            migration_hash VARCHAR(32) NOT NULL
+    );`);
+
+    logger.info("migrations table created");
+
+    logger.info("done");
 
     process.exit(0);
 })();

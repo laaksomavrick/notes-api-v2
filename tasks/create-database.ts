dc3efdde352767b3f212610b6ae54fc1f7388c75
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-10-19 19:17:27 -0400

    feature/db_migrate

--- tasks/create-database.ts
+++ tasks/create-database.ts
@@ -3,30 +3,49 @@ import { LoggerFactory } from "../lib/logger";
 
 (async (): Promise<void> => {
     const logger = LoggerFactory.getLogger();
-    // TODO: refactor config into lib/config alongside src/index.ts
-    const database = new Database({
-        database: "postgres",
-        host: "localhost",
-        port: 5432,
-        user: "postgres",
-    });
+    let postgresDb;
+    let notesDb;
+    try {
+        // TODO: refactor config into lib/config alongside src/index.ts
+        postgresDb = new Database({
+            database: "postgres",
+            host: "localhost",
+            port: 5432,
+            user: "postgres",
+        });
 
-    await database.init();
+        await postgresDb.init();
 
-    await database.query("CREATE DATABASE notes;");
+        await postgresDb.query("CREATE DATABASE notes;");
 
-    logger.info("notes database created");
+        logger.info("notes database created");
 
-    await database.query(`
-        CREATE TABLE notes.migrations(
+        notesDb = new Database({
+            database: "notes",
+            host: "localhost",
+            port: 5432,
+            user: "postgres",
+        });
+
+        await notesDb.query(`
+        CREATE TABLE migrations(
             id serial PRIMARY KEY,
             migration_name VARCHAR(256) NOT NULL,
             migration_hash VARCHAR(32) NOT NULL
     );`);
 
-    logger.info("migrations table created");
+        logger.info("migrations table created");
 
-    logger.info("done");
-
-    process.exit(0);
+        logger.info("done");
+    } catch (e) {
+        logger.error("something went wrong", e);
+    } finally {
+        if (postgresDb) {
+            await postgresDb.disconnect();
+        }
+        if (notesDb) {
+            await notesDb.disconnect();
+        }
+        process.exit(0);
+    }
 })();
---
+++ tasks/migrate-database.ts
@@ -0,0 +1,74 @@
+import crypto from "crypto";
+import fs from "fs";
+import path from "path";
+import { Database } from "../lib/database";
+import { LoggerFactory } from "../lib/logger";
+
+const MIGRATIONS_DIRECTORY = path.join(process.cwd(), "migrations");
+
+(async (): Promise<void> => {
+    const logger = LoggerFactory.getLogger();
+    let notesDb: Database;
+    try {
+        logger.info("running migrations");
+
+        // TODO: refactor config into lib/config alongside src/index.ts
+        notesDb = new Database({
+            database: "notes",
+            host: "localhost",
+            port: 5432,
+            user: "postgres",
+        });
+
+        const filenames = await fs.promises.readdir(MIGRATIONS_DIRECTORY);
+        const migrationsInFolder = filenames.map((filename: string) => ({
+            filename,
+            hash: crypto
+                .createHash("md5")
+                .update(filename)
+                .digest("hex"),
+        }));
+
+        const migrationsInDb = await notesDb.queryRows(`
+            SELECT *
+            FROM migrations
+            ORDER BY id
+        `);
+
+        for (const [i, migrationInDb] of migrationsInDb.entries()) {
+            const match = migrationsInFolder[i];
+            // @ts-ignore
+            if (match.hash !== migrationInDb.migration_hash) {
+                throw new Error(`Unknown migration found: ${JSON.stringify(match)}`);
+            }
+        }
+
+        for (const [index, { filename, hash }] of migrationsInFolder.entries()) {
+            const alreadyRan = migrationsInDb[index];
+            if (alreadyRan) {
+                // @ts-ignore
+                logger.info("skipping", alreadyRan.migration_name);
+                continue;
+            }
+
+            const filepath = path.join(MIGRATIONS_DIRECTORY, filename);
+            const sql = await fs.promises.readFile(filepath, "utf8");
+
+            await notesDb.query(sql);
+            await notesDb.query(
+                `
+                INSERT INTO migrations (migration_name, migration_hash) VALUES ($1, $2)
+            `,
+                [filename, hash],
+            );
+
+            logger.info("ran migration", filename);
+        }
+    } catch (e) {
+        logger.error("something went wrong", e);
+        process.exit(1);
+    } finally {
+        logger.info("done");
+        process.exit(0);
+    }
+})();

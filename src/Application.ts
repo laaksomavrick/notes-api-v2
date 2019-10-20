e00d998ca9652d66a0f00b56424f40c61b59c20e
Mavrick Laakso <laakso.mavrick@gmail.com>
2019-10-20 15:12:06 -0400

    chore/app-oop (#16)

---
+++ src/Application.ts
@@ -0,0 +1,44 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express, { Express, Router } from "express";
+import { ServerConfig } from "../lib/config";
+import { Database } from "../lib/database";
+import { LoggerFactory } from "../lib/logger";
+import { helloWorldHandler } from "./handlers";
+
+export class Application {
+    private server: Express;
+
+    private database: Database;
+
+    private config: ServerConfig;
+
+    private logger = LoggerFactory.getLogger();
+
+    constructor(database: Database, config: ServerConfig) {
+        this.server = this.buildApp();
+        this.database = database;
+        this.config = config;
+    }
+
+    public async serve(): Promise<void> {
+        await this.database.init();
+        this.server.listen(this.config.port, () => {
+            this.logger.info("app listening on 3000");
+        });
+    }
+
+    // TODO: may want to pull this out at some point
+    private buildApp(): Express {
+        const app = express();
+        app.use(bodyParser.urlencoded({ extended: true }));
+        app.use(bodyParser.json());
+        app.use(cors());
+
+        const router = Router();
+        router.get("*", helloWorldHandler);
+
+        app.use(router);
+        return app;
+    }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,11 @@
-import bodyParser from "body-parser";
-import cors from "cors";
-import express, { Router } from "express";
 import { DatabaseConfig, ServerConfig } from "../lib/config";
 import { Database } from "../lib/database";
-import { LoggerFactory } from "../lib/logger";
-import { helloWorldHandler } from "./handlers";
+import { Application } from "./Application";
 
-const logger = LoggerFactory.getLogger();
-const serverConfig = new ServerConfig();
 const databaseConfig = new DatabaseConfig();
-
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(cors());
-
-const router = Router();
-router.get("*", helloWorldHandler);
-
-app.use(router);
-
 const database = new Database(databaseConfig);
 
-(async (): Promise<void> => {
-    await database.init();
-    app.listen(serverConfig.port, () => {
-        logger.info("app listening on 3000");
-    });
-})();
+const serverConfig = new ServerConfig();
+const app = new Application(database, serverConfig);
+
+app.serve();

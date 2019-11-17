305dd67530b089c0526853df190752040a7d0ecb
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2019-11-17 15:16:10 -0500

    Implemented postgres image for circleci; updated create and migrate tasks

--- tasks/create-database.ts
+++ tasks/create-database.ts
@@ -3,8 +3,11 @@ import { LoggerFactory } from "../lib/logger";
 
 (async (): Promise<void> => {
     const logger = LoggerFactory.getLogger();
+    const dbName = "notes";
+
     let postgresDb;
     let notesDb;
+
     try {
         // TODO: refactor config into lib/config alongside src/index.ts
         postgresDb = new Database({
@@ -17,12 +20,19 @@ import { LoggerFactory } from "../lib/logger";
 
         await postgresDb.init();
 
-        // TODO: check that db doesn't already exist, fail gracefully if so
-        // TODO: differentiate between dev and test
+        const existsQuery = await postgresDb.query(`
+            SELECT 1 AS exists FROM pg_database WHERE datname='${dbName}'
+        `);
 
-        await postgresDb.query("CREATE DATABASE notes;");
+        const [existsQueryResult] = existsQuery.rows;
+        const exists = existsQueryResult.exists;
 
-        logger.info("notes database created");
+        if (exists === 1) {
+            logger.info(`${dbName} already exists, skipping creation`);
+        } else {
+            await postgresDb.query(`CREATE DATABASE ${dbName};`);
+            logger.info(`${dbName} database created`);
+        }
 
         notesDb = new Database({
             database: "notes",
@@ -33,17 +43,17 @@ import { LoggerFactory } from "../lib/logger";
         });
 
         await notesDb.query(`
-        CREATE TABLE migrations(
+        CREATE TABLE IF NOT EXISTS migrations(
             id serial PRIMARY KEY,
             migration_name VARCHAR(256) NOT NULL,
             migration_hash VARCHAR(32) NOT NULL
     );`);
 
         logger.info("migrations table created");
-
         logger.info("done");
     } catch (e) {
         logger.error("something went wrong", e);
+        process.exit(1);
     } finally {
         if (postgresDb) {
             await postgresDb.disconnect();

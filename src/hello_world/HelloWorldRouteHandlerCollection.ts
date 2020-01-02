19743ee2501e964cbda20da28e488f579e1e69d0
laakso.mavrick@gmail.com <laakso.mavrick@gmail.com>
2020-01-01 20:37:23 -0500

    Updated timestamp log for hello world handler

--- src/hello_world/HelloWorldRouteHandlerCollection.ts
+++ src/hello_world/HelloWorldRouteHandlerCollection.ts
@@ -3,9 +3,11 @@ import { RouteHandlerCollection } from "../framework/RouteHandlerCollection";
 
 export class HelloWorldRouteHandlerCollection extends RouteHandlerCollection {
     public build(): void {
-        const { app } = this;
+        const { app, logger } = this;
 
         app.get("*", (req: Request, res: Response): void => {
+            const now = new Date();
+            logger.info(`Received request at ${now}`);
             res.send("Hello, world");
         });
     }
